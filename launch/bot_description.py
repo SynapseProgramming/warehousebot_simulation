385802341e40e55886861c2ca5e3c8bf8fe1f560
Roald Ong <roald.ong@gmail.com>
2023-07-05 17:26:45 +0800

    Added in joint state publisher in launch file

--- launch/bot_description.py
+++ launch/bot_description.py
@@ -11,67 +11,61 @@ from launch_ros.actions import Node
 from launch.conditions import IfCondition, UnlessCondition
 
 
-
 def generate_launch_description():
+    robot_model = "warehouse_bot_sim.urdf"
+    current_dir = get_package_share_directory("warehousebot_simulation")
+    robot_model_path = os.path.join(current_dir, "urdf", robot_model)
 
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    rviz_config_dir = LaunchConfiguration("rviz_config_dir")
+    run_rviz2 = LaunchConfiguration("run_rviz2")
 
-
-    robot_model='warehouse_bot_sim.urdf'
-    current_dir=get_package_share_directory('warehousebot_simulation')
-    robot_model_path=os.path.join(current_dir,'urdf',robot_model)
-
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    rviz_config_dir=LaunchConfiguration('rviz_config_dir')
-    run_rviz2=LaunchConfiguration('run_rviz2')
-
-
-    with open(robot_model_path, 'r') as infp:
+    with open(robot_model_path, "r") as infp:
         robot_desc = infp.read()
 
-
-    declare_sim_time=DeclareLaunchArgument(
-                'use_sim_time',
-                default_value='false',
-                description='Use simulation (Gazebo) clock if true')
-
-
-    declare_rviz_launch=DeclareLaunchArgument(
-                'run_rviz2',
-                default_value='false',
-                description='run rviz')
-
-
-    declare_rviz_config=DeclareLaunchArgument(
-                'rviz_config_dir',
-                default_value=os.path.join(current_dir,'rviz','view_warehousebot.rviz'),
-                description='default path to rviz config file')
-
-
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{
-            'use_sim_time': use_sim_time,
-            'robot_description': robot_desc
-        }]
+    declare_sim_time = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
     )
 
-    ld=LaunchDescription()
+    declare_rviz_launch = DeclareLaunchArgument(
+        "run_rviz2", default_value="false", description="run rviz"
+    )
 
-    launch_rviz2=Node(
-                condition=IfCondition(run_rviz2),
-                package='rviz2',
-               executable='rviz2',
-                name='rviz2',
-                arguments=['-d', rviz_config_dir],
-                parameters=[{'use_sim_time': use_sim_time}],
-                output='screen')
+    declare_rviz_config = DeclareLaunchArgument(
+        "rviz_config_dir",
+        default_value=os.path.join(current_dir, "rviz", "view_warehousebot.rviz"),
+        description="default path to rviz config file",
+    )
 
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"use_sim_time": use_sim_time, "robot_description": robot_desc}],
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+    )
+
+    ld = LaunchDescription()
+
+    launch_rviz2 = Node(
+        condition=IfCondition(run_rviz2),
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_dir],
+        parameters=[{"use_sim_time": use_sim_time}],
+        output="screen",
+    )
 
     ld.add_action(declare_rviz_launch)
     ld.add_action(declare_sim_time)
     ld.add_action(declare_rviz_config)
     ld.add_action(launch_rviz2)
     ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
     return ld
